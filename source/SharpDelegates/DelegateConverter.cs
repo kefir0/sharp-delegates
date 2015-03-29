2234b6618a7a9179b64b96c953f7028cb3ea2979
Pavel Kefir <naxah1@gmail.com>
2015-03-29 10:40:00 +0300

    Expression tree conversion added.

--- source/SharpDelegates/DelegateConverter.cs
+++ source/SharpDelegates/DelegateConverter.cs
@@ -1,26 +1,41 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace SharpDelegates
 {
     public class DelegateConverter
     {
-        /// <summary>
-        /// Gets the Func delegate that calls specified method on specified target object.
-        /// </summary>
-        /// <param name="target">The target object.</param>
-        /// <param name="resultType">Type of the method result.</param>
-        /// <param name="methodName">Name of the method; defaults to "Invoke".</param>
-        /// <returns>Func delegate that calls specified method on specified target object.</returns>
+        // http://elegantcode.com/2010/07/30/createdelegatet-an-exercise-in-using-expressions/
+
+        private const string DefaultMethodName = "Invoke";
+
         public static Func<object> GetFunc(object target, Type resultType, string methodName = null)
         {
-            methodName = methodName ?? "Invoke";
-
-            var delegateType = typeof(Func<>).MakeGenericType(resultType);
-
-            var dg = Delegate.CreateDelegate(delegateType, target, methodName);
+            var dg = CreateDelegate(target, resultType, methodName);
 
             return () => dg.DynamicInvoke();
         }
 
+        public static Delegate CreateDelegate(object target, Type resultType, string methodName = null)
+        {
+            methodName = methodName ?? DefaultMethodName;
+
+            var delegateType = typeof (Func<>).MakeGenericType(resultType);
+
+            var dg = Delegate.CreateDelegate(delegateType, target, methodName);
+            return dg;
+        }
+
+        public static Func<object> GetFuncUsingExprTree(object target, string methodName = null)
+        {
+            Expression<Func<object>> instExpr = () => target;
+            var targetType = target.GetType();
+            var convertedInstanceExpression = Expression.Convert(instExpr.Body, targetType);
+            var method = targetType.GetMethod(methodName ?? DefaultMethodName);
+            var callExpr = Expression.Call(convertedInstanceExpression, method);
+            var convertResultExpr = Expression.Convert(callExpr, typeof (object));
+            var lambda = Expression.Lambda<Func<object>>(convertResultExpr);
+            return lambda.Compile();
+        }
     }
 }
--- source/SharpDelegatesTests/GetFuncTest.cs
+++ source/SharpDelegatesTests/GetFuncTest.cs
@@ -23,30 +23,44 @@ namespace SharpDelegatesTests
         {
             Func<DateTime> func = () => DateTime.Now;
 
+            // Raw
+            Profile(() => func()); // warmup
+            var rawTime = Profile(() => func());
+            
+            // Converted
+            var wrappedFunc = DelegateConverter.GetFunc(func, typeof(DateTime));
+            var convertedTime = Profile(() => wrappedFunc());
+            
+            // Converted and cast
+            var dlgate = DelegateConverter.CreateDelegate(func, typeof(DateTime));
+            var castFunc = (Func<DateTime>)dlgate;
+            var castTime = Profile(() => castFunc());
+            
+            // Dynamic
+            var dyn = (dynamic) dlgate;
+            var dynamicTime = Profile(() => dyn.Invoke());
+
+            // Expression tree
+            var exprTree = DelegateConverter.GetFuncUsingExprTree(func);
+            var exprTreeTime = Profile(() => exprTree());
+
+            Console.WriteLine(rawTime + " " + convertedTime + " " + castTime + " " + dynamicTime + " " + exprTreeTime);
+            Console.WriteLine("Converted: " + (double)convertedTime/rawTime);
+            Console.WriteLine("Cast: " + (double)castTime/rawTime);
+            Console.WriteLine("Dynamic: " + (double)dynamicTime/rawTime);
+            Console.WriteLine("Expr Tree: " + (double)exprTreeTime/rawTime);
+        }
+
+        private static long Profile(Action action)
+        {
             const int testSize = 100000;
 
-            // Raw
             var sw = Stopwatch.StartNew();
 
-            for (var i = 0; i < testSize; i++)
-                Assert.IsNotNull(func());
+            for (int i = 0; i < testSize; i++)
+                action();
 
-            var rawTime = sw.ElapsedTicks;
-
-            var wrappedFunc = DelegateConverter.GetFunc(func, typeof (DateTime));
-
-
-            // Converted
-            sw = Stopwatch.StartNew();
-
-            for (var i = 0; i < testSize; i++)
-                Assert.IsNotNull(wrappedFunc());
-
-            var convertedTime = sw.ElapsedTicks;
-
-            Console.WriteLine(rawTime);
-            Console.WriteLine(convertedTime);
-            Console.WriteLine(convertedTime/rawTime);
+            return sw.ElapsedTicks;
         }
     }
 }
